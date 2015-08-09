a6fef3ece1fd143164616303f686cb89a759b484
shan-sundaram <shan.sundaram@ctl.io>
2015-08-09 11:43:31 -0500

    Moved content-type header to interceptor service

--- app/services/wfaasService.js
+++ app/services/wfaasService.js
@@ -11,11 +11,8 @@
             jobAPI.getAllJobs = function(){
                 return $http({
                     method: 'GET',
-                    url: 'http://10.121.41.26:8080/jobs/wfad', //Dev url
+                    url: 'http://10.121.41.26:8080/jobs/wfad' //Dev url
 //                    url: 'http://64.15.188.230/jobs/wfaq', //QA url
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
                 });
             }
 
@@ -23,25 +20,18 @@
                 return $http({
                     method: 'PUT',
                     url: 'http://10.121.41.26:8080/jobs/wfad?immediate=' + immediate, //Dev url
-                    data: jobData,
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
+                    data: jobData
                 });
             }
             jobAPI.startJob = function(jobId){
                 return $http({
                     method: 'POST',
                     url: 'http://10.121.41.26:8080/jobs/wfad/' + jobId + '/start', //Dev url
-                    data: '{}',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
+                    data: '{}'
                 });
             }
             return jobAPI;
 
-            /*TODO - CLC Login Authentication and use it for any subsequent API request*/
         }])
         .factory('statusAPIService',['$http',function($http){
             var statusAPI = {};
@@ -49,29 +39,21 @@
             statusAPI.getJobDetails = function(jobId){
                 return $http({
                     method: 'GET',
-                    url: 'http://10.121.41.26:8084/status/wfad/job/' + jobId, //Dev url
+                    url: 'http://10.121.41.26:8084/status/wfad/job/' + jobId //Dev url
 //                    url: 'http://64.15.188.230/status/wfad/job/' + jobId, //QA url
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
                 });
             }
             return statusAPI;
-
-            /*TODO - CLC Login Authentication and use it for any subsequent API request*/
-        }])
-        .factory('clcAPIService',['$http', function($http){
-            var clcAPI = {};
-
-            clcAPI.getAuthenticationToken = function(loginData){
-                return $http({
-                    method: 'POST',
-                    url: 'https://api.ctl.io/v2/authentication/login',
-                    data: loginData,
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                });
-            }
-            return  clcAPI;
         }]);
+        // .factory('clcAPIService',['$http', function($http){
+        //     var clcAPI = {};
+
+        //     clcAPI.getAuthenticationToken = function(loginData){
+        //         return $http({
+        //             method: 'POST',
+        //             url: 'https://api.ctl.io/v2/authentication/login',
+        //             data: loginData
+        //         });
+        //     }
+        //     return  clcAPI;
+        // }]);
