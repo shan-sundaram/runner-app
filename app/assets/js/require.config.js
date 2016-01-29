8cc38609110d0630a2dae27218170d817f3e84a6
brianalbrecht <brian.albrecht@centurylink.com>
2016-01-29 10:58:58 -0600

    fixed issue with Cyclops / KO injection

--- app/assets/js/require.config.js
+++ app/assets/js/require.config.js
@@ -7,14 +7,6 @@
 console.log('require.config.js');
 
 
-
-window.ko = require(['knockout']);
-window.$ = require(['jquery']);
-//window.$.ui = require(['jqueryUi']);
-window.ko.validation = require(['knockoutValidation']);
-//window.moment = require(['moment']);
-
-
 require.config({
     // Initialize the application with the main application file.
     baseUrl: 'assets/js',
@@ -41,59 +33,16 @@ require.config({
     },
 
     shim: {
-        //'cyclops': ['knockout', 'jquery']
-        //'cyclops': {
-        //    deps: ['jquery', 'knockout'],
-        //    exports: 'cyclops'
-        //},
         "cyclops": {
             deps: ["jquery", 'knockout']
         }
-        //'jquery.actionToolbarConfirm': {
-        //    deps: ['jquery', 'cyclops'],
-        //    exports: 'jQuery.fn.actionToolbarConfirm'
-        //}
-
-
     },
 
-    //shim: {
-    //    //'cyclops': ['knockout', 'jquery']
-    //    'cyclops': {
-    //        deps: ['jquery', 'knockout'],
-    //        exports: 'cyclops'
-    //    },
-    ////    //'jquery.actionToolbarConfirm': {
-    ////    //    deps: ['jquery'],
-    ////    //    exports: 'jQuery.fn.actionToolbarConfirm'
-    ////    //},
-    ////    //
-    //    'jquery': {
-    //        exports: '$'
-    //    },
-    //    'knockout': {
-    //        exports: 'ko'
-    //    }
-    //},
     deps: ['main']
 });
 
-
-
-
-
-//require(['https://assets.ctl.io/cyclops/1.1.3/scripts/cyclops.js', 'knockout'], function(cyclops, ko){
-//    console.log('cyclops start');
-//
-//});
-
-//define('jquery', function () { return jQuery; });
-//define('knockout', ko);
-
-
-  //<script src='https://code.jquery.com/jquery-2.1.4.min.js' ></script>
-  //<script src='https://ajax.aspnetcdn.com/ajax/knockout/knockout-3.3.0.js' ></script>
-  //<script src='https://cdnjs.cloudflare.com/ajax/libs/knockout-validation/2.0.3/knockout.validation.min.js' ></script>
-  //<script src='https://code.jquery.com/ui/1.11.4/jquery-ui.min.js'></script>
-  //<script src='https://assets.ctl.io/cyclops/1.1.2/scripts/cyclops.min.js' ></script>
-  //<script src='./scripts/site.js' ></script>
+// Set a global ko object after Knockout is loaded so that
+// Cyclops can use it.
+require(['knockout'], function(ko){
+    window.ko = ko;
+});
